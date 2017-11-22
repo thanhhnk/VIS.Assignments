f15808f2ea4081b2a3dd7dd21fa45b8c70c0e194
thanhhnk <thanhhnk@gmail.com>
2017-11-22 15:04:06 +0100

    Changed structures

---
+++ Vis.Ass06/img_trans/CMakeFiles/img_transf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/img_transf.dir/main.cpp.o"
+  "img_transf.pdb"
+  "img_transf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/img_transf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
